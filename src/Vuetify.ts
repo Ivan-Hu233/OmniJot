6e6540f6d0e0a5b51a278c8ce062f15fb73bde99
Ivan-Hu233 <2917822657@qq.com>
2026-08-13 15:03:57 +0800

    chore:修改vuetify导入方式

--- src/Vuetify.ts
+++ src/Vuetify.ts
@@ -1,6 +1,4 @@
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import 'vuetify/styles'
 
@@ -20,8 +18,6 @@ const vuetify = createVuetify({
       density: 'comfortable',
     },
   },
-  components,
-  directives,
 })
 
 export default vuetify
